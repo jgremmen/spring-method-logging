9a82c33a18a29fb11a61595a48444cfc8e0b9ea9
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2024-05-23 07:31:32 +0200

    fix copyright year

--- src/main/java/de/sayayi/lib/methodlogging/logger/LogbackLogger.java
+++ src/main/java/de/sayayi/lib/methodlogging/logger/LogbackLogger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Jeroen Gremmen
+ * Copyright 2024 Jeroen Gremmen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
