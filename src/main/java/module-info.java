70203ae880b1cccc4d9383e920d3a99fc1ce7b1d
Jeroen Gremmen <jeroen.gremmen@sayayi.de>
2025-06-08 14:45:07 +0200

    add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module de.sayayi.lib.methodlogging
+{
+  requires de.sayayi.lib.message;
+  requires spring.aop;
+  requires spring.beans;
+  requires spring.context;
+  requires spring.core;
+
+  requires static java.logging;
+  requires static ch.qos.logback.classic;
+  requires static ch.qos.logback.core;
+  requires static org.apache.logging.log4j;
+  requires static org.jetbrains.annotations;
+  requires static org.slf4j;
+  requires static spring.jcl;
+
+  exports de.sayayi.lib.methodlogging;
+  exports de.sayayi.lib.methodlogging.annotation;
+  exports de.sayayi.lib.methodlogging.internal;
+  exports de.sayayi.lib.methodlogging.logger;
+}
